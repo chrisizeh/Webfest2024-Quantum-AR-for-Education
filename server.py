9eda0ed54a6664a392839f33a19b668bb17fdf45
chrisizeh <me@christinezeh.com>
2024-08-10 15:41:13 +0200

    added cat

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, redirect
 
-app = Flask(__name__)
+app = Flask(__name__,  static_folder='static')
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
