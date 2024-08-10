aa3d2fb35ec786a7f5d495c873ba2c719c40c395
proman3419 <proman7268@gmail.com>
2024-08-10 14:31:34 +0200

    pip3?

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # For development use (simple logging, etc):
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 server.py
 # For production use:
 # gunicorn server:app -w 1 --log-file -
